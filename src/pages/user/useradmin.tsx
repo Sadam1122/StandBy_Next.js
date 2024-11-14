8a0865a79ba6b2d1808c2d1e7510065e75abc776
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2024-11-14 22:39:23 +0700

    Update useradmin.tsx

--- src/pages/user/useradmin.tsx
+++ src/pages/user/useradmin.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/navigation';
 import supabase from '../../components/SupabaseClient';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
-import Image from 'next/image';
 
 type User = {
   id: string;
@@ -217,13 +216,7 @@ const CreateUserPage = () => {
               {users.map((user) => (
                 <tr key={user.id}>
                   <td className="px-6 py-4 whitespace-nowrap">
-                  <Image
-                      src={user.avatar_url}
-                      alt="Avatar"
-                      width={40}
-                      height={40}
-                      className="rounded-full"
-                    />
+                    <img src={user.avatar_url} alt="Avatar" className="h-10 w-10 rounded-full" />
                   </td>
                   <td className="px-6 py-4 whitespace-nowrap">{user.full_name}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{user.email}</td>
