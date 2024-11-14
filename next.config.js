f758fc6c5805abc8c4ea71abed90626fb41d731e
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2024-11-14 23:31:12 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['uoaqpcsbemezoyfvrpbf.supabase.co'], // Add the Supabase domain here
+  },
+};
+
+module.exports = nextConfig;
