5741b5155c32544fb5ec5735f87b2e9003bd5299
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2024-12-13 10:00:42 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-"use client";
-import { useState } from 'react';
+"use client"; 
+
+import supabase from '../components/SupabaseClient'; 
 import { useRouter } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import logo from '../assets/standbyputih.png';
 import koran from '../assets/koran.png';
@@ -11,80 +13,75 @@ const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);  
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false); 
   const router = useRouter();
 
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      const { error } = await supabase
+        .from('profiles')
+        .select('full_name, avatar_url')
+        .eq('id', user?.id) 
+        .single();
+  
+      if (error) {
+        setError('Error fetching profile');
+      }
+    };
+  
+    checkUser();
+  }, [router]);
+  
+  
   const handleLogin = async () => {
-    if (!email || !password) {
-      setError("Email dan Password tidak boleh kosong");
-      return;  
-    }
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
-    setError('');
-    
-    try {
-      const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      
-      if (!response.ok) {
-        console.error('Login failed:', response.status, await response.text()); // Menampilkan pesan error dari server
-        setError("Login gagal. Silakan coba lagi nanti.");
-        return;
-      }
-      
-      const data = await response.json();
-      if (response.ok) {
-        router.push('/home'); // Redirect ke halaman home setelah login berhasil
-      } else {
-        setError(data.error || "Login gagal. Silakan periksa kembali email atau password Anda.");
-      }
-      
-    } catch (err) {
-      console.error('Login error:', err); // Mencatat error ke console untuk debugging
-      setError("Terjadi kesalahan, coba lagi nanti.");
+    if (error) {
+      setError(error.message);
+    } else {
+      const { data: { session } } = await supabase.auth.getSession();
+      document.cookie = `token=${session?.access_token}; path=/;`;
+      router.push('/home');
     }
   };
 
   const togglePasswordVisibility = () => {
-    setIsPasswordVisible(!isPasswordVisible);
+    setIsPasswordVisible(!isPasswordVisible); 
   };
 
   return (
     <div className="flex min-h-screen bg-[#991b1b] relative z-20">
       <div className="w-1/2 bg-[#991b1b] flex items-center justify-center flex-col p-8">
-        <Image src={logo} alt="Logo" className="mb-4 w-32 h-32" />
+        <Image src={logo} alt="Logo" className="mb-4 w-32 h-32" /> 
         <h1 className="text-white text-2xl font-bold">Selamat Datang di StandBy</h1>
         <p className="text-white mt-2">Mendata hasil inkubator sesuai standarisasi & dokumentasi</p>
-        <Image src={koran} alt="Document" className="mt-6 w-32 h-32" />
+        <Image src={koran} alt="Document" className="mt-6 w-32 h-32" /> 
       </div>
       <div className="w-1/2 flex items-center justify-center bg-[#F7F7F7]">
         <div className="bg-white p-6 rounded shadow-md w-80">
           <h2 className="text-black text-lg font-semibold mb-4">Masuk Akun</h2>
-          {error && <p className="text-red-500 text-center mb-4">{error}</p>}  
+          {error && <p className="text-red-500 text-center mb-4">{error}</p>}
           <input
-            type="email"  
-            placeholder="Masukkan email Anda"
+            type="text"
+            placeholder="Masukkan nama Anda"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="border border-gray-300 rounded-lg p-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300 text-black"
-            required
+            className="border border-gray-300 rounded-lg p-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
           />
           <div className="relative mb-4">
             <input
-              type={isPasswordVisible ? 'text' : 'password'}
+              type={isPasswordVisible ? 'text' : 'password'} 
               placeholder="Masukkan kata sandi Anda"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="border border-gray-300 rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300 text-black"
-              required
+              className="border border-gray-300 rounded-lg p-3 w-full focus:outline-none focus:ring-2 focus:ring-red-500 transition duration-300"
             />
-            <button
-              onClick={togglePasswordVisibility}
+            <button 
+              onClick={togglePasswordVisibility} 
               className="absolute right-3 top-3 text-gray-500 focus:outline-none"
               type="button"
             >
@@ -95,9 +92,8 @@ const LoginPage = () => {
               )}
             </button>
           </div>
-       
-          <button
-            onClick={handleLogin}
+          <button 
+            onClick={handleLogin} 
             className="bg-[#991b1b] text-white rounded-lg p-3 w-full hover:bg-red-700 transition duration-300"
           >
             Masuk
@@ -106,6 +102,6 @@ const LoginPage = () => {
       </div>
     </div>
   );
-};
+}
 
 export default LoginPage;
