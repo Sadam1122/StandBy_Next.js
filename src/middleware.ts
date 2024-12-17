1b4d1591fe9a828746c0ba217e55caee5eb56bcd
FakhriadiRasyaad <161245848+FakhriadiRasyaad@users.noreply.github.com>
2024-12-17 18:41:36 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,10 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token'); // Mengambil token dari cookies
 
 
-  const publicPaths = ['/'];
+  const publicPaths = ['/login', '/'];
 
   if (!token && !publicPaths.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   return NextResponse.next(); 
